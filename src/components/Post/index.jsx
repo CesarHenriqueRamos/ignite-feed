f3d106f225cc56e650eb13fb80a65cf6fd8e87ee
Cesar Henrique Ramos <cesar252742@gmail.com>
2023-08-24 09:17:07 -0300

    criado o componente de post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,6 +1,22 @@
+import styles from './Post.module.css';
 
 export function Post(){
     return(
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eum vero recusandae, porro atque at fuga facere esse. Doloribus fugit, cupiditate temporibus illo voluptatum recusandae accusantium dolores asperiores ipsa. Voluptatibus.</p>
-    )
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/27959551?v=4" />
+                    <div className={styles.authorInfo}>
+                        <strong>Cesar Henrique Ramos</strong>
+                        <span>Developer Full Stack</span>
+                    </div>
+                </div>
+                <time title='22 de Agosto as 14:49:00' dateTime='2023-08-22 14:49:00'>Publicado a uma hora</time>
+            </header>
+            <div className={styles.content}>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, voluptates iusto! Dolores beatae repudiandae sint fugiat eum sequi eligendi fuga reiciendis aperiam tenetur animi in a, magnam pariatur voluptate quia.</p>
+                <p><a href="">Links</a></p>
+            </div>
+        </article>
+        )
 }
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from '@phosphor-icons/react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -5,11 +6,12 @@ export function Sidebar(){
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=40" />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/27959551?v=4" />
                 <strong>Cesar Henrique Ramos</strong>
                 <span>Development Full Stack</span>
             </div>
             <footer>
-                <a href="#"> Editar seu Perfil</a>
+                <a href="#"><PencilLine size={20} /> Editar seu Perfil</a>
             </footer>
         </aside>
     )
